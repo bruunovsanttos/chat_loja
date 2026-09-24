e8edcf91df11c95178e511ef402ccdc0adfbe546
bruunosanttos <bruno-vieira-santos@hotmail.com>
2026-09-24 15:55:37 -0300

    feat:criação de configuração da api

---
+++ app/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = "sqlite:///loja.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
